43f18872513d5c235c2f685ae174cad0b2537217
jianxunliu <jianxunliu@meilishuo.com>
2015-07-24 19:54:51 +0800

    添加文案注释

---
+++ awk_mail.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+p=$1
+awk -v p="${p:=5}" 'BEGIN{ FS="\n";RS="";ORS="\n" } {
+    for(i=1;i<=NF;i++){
+        if(i%p==0 && i!=NF ){
+            print $i"\n"
+        }else{
+            print $i" end here"    
+        }
+    } 
+}' ./mail.log
--- date.sh
+++ date.sh
@@ -11,22 +11,11 @@
 
 #sed -e 's/ //;s/:/ /;s/\// /g' ./sjm.txt | awk -F ' ' '{print "'`date +%Y-%m-%d -d "$1"`'" $2}'
 #sed -e 's/ //;s/:/ /;s/\//-/g' ./sjm.txt | awk -F ' ' '{print "'`date '+%Y-%m-%d' -d "$d"`' "$2}' 
-sed -e 's/ //;s/:/ /;s/\//-/g' ./sjm.txt | awk -F ' ' '{system("date +%Y-%m-%d -d " $1);print $2"\n"}' | awk 'BEGIN{ FS="\n";RS="";OFS=" ";ORS="\n"} {print $1,$2}'
-exit 0;
 
+#方法1
+#sed -e 's/ //;s/:/ /;s/\//-/g' ./sjm.txt | awk -F ' ' '{system("date +%Y-%m-%d -d " $1);print $2"\n"}' | awk 'BEGIN{ FS="\n";RS="";OFS=" ";ORS="\n"} {print $1,$2}'
+#exit 0;
 
-
-str=`sed -e 's/ //;s/:/ /;s/\// /g' ./sjm.txt`
-echo $str
+#方法2
+sed -e 's/ //;s/:/ /;s/\//-/g' ./sjm.txt | awk -F ' ' '{"date +%Y-%m-%d -d" $1|getline d;print d" "$2}'
 exit 0
-
-
-for((i=0;i<=10;i++))
-do
-    echo str[$i]."--------"
-done
-exit 0
-
-
-echo $str
-#awk -F '+' '{print $1 $2}' | $str
